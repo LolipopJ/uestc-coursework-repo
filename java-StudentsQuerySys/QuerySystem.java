9f45980f32a1169268baba6e83274656a4961ae4
宋璟珅 <lolipop0703@qq.com>
2019-12-14 12:02:26 +0800

    update

--- java-StudentsQuerySys/QuerySystem.java
+++ java-StudentsQuerySys/QuerySystem.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 /**
  * @author Lolipop
- * @lastUpdate 2019/12/8
+ * @lastUpdate 2019/12/14
  */
 public class QuerySystem {
     private static Scanner scanner = new Scanner(System.in);
 
-    // 在这里修改数据存储目录
-    private static Myfile file = new Myfile("H:\\Lolipop\\JAVA_STUDY_EXP");
-    // 设置MAXSIZE
+    // 文件默认存储在工作目录
+    private static Myfile file = new Myfile(System.getProperty("user.dir"));
+
+    // 设置最大存储数MAXSIZE
     private static int MAXSIZE = file.setMAXSIZE(30);
 
     // 初始化变量
@@ -36,13 +37,13 @@ public class QuerySystem {
     private static Electivecourse[] electivecourses = new Electivecourse[MAXSIZE];
 
     // choice选项值
-    final private static int quitCase = 0;
-    final private static int queryInfoCase = 1;
-    final private static int insertStudentCase = 2;
-    final private static int insertTeacherCase = 3;
-    final private static int insertCourseCase = 4;
-    final private static int insertScheduleCase = 5;
-    final private static int insertElectivecourseCase = 6;
+    final private static int QUIT_CASE = 0;
+    final private static int QUERY_INFO_CASE = 1;
+    final private static int INSERT_STUDENT_CASE = 2;
+    final private static int INSERT_TEACHER_CASE = 3;
+    final private static int INSERT_COURSE_CASE = 4;
+    final private static int INSERT_SCHEDULE_CASE = 5;
+    final private static int INSERT_ELECTIVECOURSE_CASE = 6;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         int choice;
@@ -52,31 +53,31 @@ public class QuerySystem {
         while(flag) {
             choice = querySys();
             switch (choice) {
-                case quitCase:
+                case QUIT_CASE:
                     System.out.println("You quit successfully!");
                     flag = false;
                     break;
-                case queryInfoCase:
+                case QUERY_INFO_CASE:
                     System.out.println("---Query student information---");
                     queryInfo();
                     break;
-                case insertStudentCase:
+                case INSERT_STUDENT_CASE:
                     System.out.println("---Insert new student information---");
                     insertStudent();
                     break;
-                case insertTeacherCase:
+                case INSERT_TEACHER_CASE:
                     System.out.println("---Insert new teacher information---");
                     insertTeacher();
                     break;
-                case insertCourseCase:
+                case INSERT_COURSE_CASE:
                     System.out.println("---Insert new course information---");
                     insertCourse();
                     break;
-                case insertScheduleCase:
+                case INSERT_SCHEDULE_CASE:
                     System.out.println("---Insert new schedule information---");
                     insertSchedule();
                     break;
-                case insertElectivecourseCase:
+                case INSERT_ELECTIVECOURSE_CASE:
                     System.out.println("---Insert new elective course information---");
                     insertElectivecourse();
                     break;
@@ -88,13 +89,13 @@ public class QuerySystem {
     // 显示操作提示界面并获取选项值
     private static int querySys() {
         System.out.println("\n---------Query Information System---------");
-        System.out.println("#"+queryInfoCase+"  Query student information");
-        System.out.println("#"+insertStudentCase+"  Insert new student information");
-        System.out.println("#"+insertTeacherCase+"  Insert new teacher information");
-        System.out.println("#"+insertCourseCase+"  Insert new course information");
-        System.out.println("#"+insertScheduleCase+"  Insert new schedule information");
-        System.out.println("#"+insertElectivecourseCase+"  Insert new elective course information");
-        System.out.println("#"+quitCase+"  Quit system");
+        System.out.println("#"+QUERY_INFO_CASE+"  Query student information");
+        System.out.println("#"+INSERT_STUDENT_CASE+"  Insert new student information");
+        System.out.println("#"+INSERT_TEACHER_CASE+"  Insert new teacher information");
+        System.out.println("#"+INSERT_COURSE_CASE+"  Insert new course information");
+        System.out.println("#"+INSERT_SCHEDULE_CASE+"  Insert new schedule information");
+        System.out.println("#"+INSERT_ELECTIVECOURSE_CASE+"  Insert new elective course information");
+        System.out.println("#"+QUIT_CASE+"  Quit system");
         System.out.println("------------------------------------------");
         System.out.print("input choice: #");
         return scanner.nextInt();
@@ -163,7 +164,7 @@ public class QuerySystem {
         Student student = new Student(name, sex, age, sid, major);
         file.writeFile(student);
 
-        readFile(insertStudentCase);
+        readFile(INSERT_STUDENT_CASE);
     }
 
     private static void insertTeacher() throws IOException, ClassNotFoundException {
@@ -181,7 +182,7 @@ public class QuerySystem {
         Teacher teacher = new Teacher(name, sex, age, tid, title);
         file.writeFile(teacher);
 
-        readFile(insertTeacherCase);
+        readFile(INSERT_TEACHER_CASE);
     }
 
     private static void insertCourse() throws IOException, ClassNotFoundException {
@@ -195,7 +196,7 @@ public class QuerySystem {
         Course course = new Course(cname, cid, chour);
         file.writeFile(course);
 
-        readFile(insertCourseCase);
+        readFile(INSERT_COURSE_CASE);
     }
 
     private static void insertSchedule() throws IOException, ClassNotFoundException {
@@ -211,7 +212,7 @@ public class QuerySystem {
         Schedule schedule = new Schedule(classid, cid, tid, classroom);
         file.writeFile(schedule);
 
-        readFile(insertScheduleCase);
+        readFile(INSERT_SCHEDULE_CASE);
     }
 
     private static void insertElectivecourse() throws IOException, ClassNotFoundException {
@@ -225,7 +226,7 @@ public class QuerySystem {
         Electivecourse electivecourse = new Electivecourse(classid, elid, sid);
         file.writeFile(electivecourse);
 
-        readFile(insertElectivecourseCase);
+        readFile(INSERT_ELECTIVECOURSE_CASE);
     }
 
     // 启动系统时读取所有文件信息
@@ -240,20 +241,22 @@ public class QuerySystem {
     // 添加信息后刷新并读取对应文件信息（增加性能）
     private static void readFile(int choice) throws IOException, ClassNotFoundException {
         switch (choice) {
-            case insertStudentCase:
+            case INSERT_STUDENT_CASE:
                 file.readFile(students);
                 break;
-            case insertTeacherCase:
+            case INSERT_TEACHER_CASE:
                 file.readFile(teachers);
                 break;
-            case insertCourseCase:
+            case INSERT_COURSE_CASE:
                 file.readFile(courses);
                 break;
-            case insertScheduleCase:
+            case INSERT_SCHEDULE_CASE:
                 file.readFile(schedules);
                 break;
-            case insertElectivecourseCase:
+            case INSERT_ELECTIVECOURSE_CASE:
                 file.readFile(electivecourses);
+                break;
+            default:
                 break;
         }
     }
